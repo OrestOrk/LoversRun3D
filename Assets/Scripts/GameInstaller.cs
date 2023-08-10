bddcf34bf1fdde8dedbff820c23ae664a0629866
Orest Kachut <orkoorest25@gmail.com>
2023-08-10 14:42:51 +0300

    AddZenject
    AddZenject

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+public class GameInstaller : MonoInstaller
+{
+    [SerializeField] private UIController _uiController;
+    [SerializeField] private PlayerMovement _playerMovement;
+    public override void InstallBindings()
+    {
+        Container.Bind<UIController>().FromInstance(_uiController).AsSingle().NonLazy();
+        Container.Bind<PlayerMovement>().FromInstance(_playerMovement).AsSingle().NonLazy();
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private int _level;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
